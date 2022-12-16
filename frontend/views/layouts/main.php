c0e75920b6d367727832d5a93dad4fa850fb3388
artmart <artrmart@gmail.com>
2022-12-16 16:21:27 +0400

    icon update

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -111,7 +111,7 @@ $user_group = Yii::$app->user->identity->user_group;
         <div class="col-md-3 left_col">
           <div class="left_col scroll-view">
             <div class="navbar nav_title" style="border: 0;">
-              <a href="/" class="site_title"><i class="fa fa-paw"></i></a>
+              <a href="/" class="site_title"><i class="fa fa-cloud"></i></a>
             </div>
 
             <div class="clearfix"></div>
